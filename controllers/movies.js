06023d164377bdb94e1f898bd144e4f3a07af20a
Roniell Perez <roniell2016@hotmail.com>
2022-11-26 14:32:47 -0400

    Feature: Variables de la vista home agregedas

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,5 +1,13 @@
+const Movie = require("../models/Movie");
+
 exports.getMoviesHome = (req, res, next) => {
-  res.render("home", {
-    title: "Movies Manager",
+  const { genre } = req.query;
+
+  Movie.getMovies((movies) => {
+    res.render("home", {
+      title: "Movies Manager",
+      movies,
+      genre,
+    });
   });
 };
